2f2c38a35faee22cc1040073de70a8b280b8a8bf
Jean-Christophe <52524271+LiSplib@users.noreply.github.com>
2020-03-23 08:08:13 +0100

    Delete db.php

--- db.php
+++
@@ -1,4 +0,0 @@
-<?php
-$pdo  =new PDO('mysql:dbname=bino;host=localhost;charset=utf8', 'root', '');
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
